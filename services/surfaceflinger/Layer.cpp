CyanogenMod/android_frameworks_native

sf: Use property to set default app buffer count
For memory optimization (based on RAM size and
screen resolution) making default app buffers count as configurable.

Change-Id: I584c33e956d56ba21af10ad1f7ad465ce506ee96

services/surfaceflinger/Layer.cpp
@@ -161,6 +161,14 @@ void Layer::onFirstRef() {
     mSurfaceFlingerConsumer->setDefaultMaxBufferCount(2);
 #else
     mSurfaceFlingerConsumer->setDefaultMaxBufferCount(3);
+
+#ifdef QCOM_BSP
+    char property[PROPERTY_VALUE_MAX];
+    if (property_get("ro.sf.default_app_buffer", property, NULL) > 0) {
+        mSurfaceFlingerConsumer->setDefaultMaxBufferCount(atoi(property));
+    }
+#endif
+
 #endif
 
     const sp<const DisplayDevice> hw(mFlinger->getDefaultDisplayDevice());

Jackeagle/caf_frameworks_native

services/surfaceflinger/Layer.cpp
@@ -154,6 +154,14 @@ void Layer::onFirstRef() {
     mSurfaceFlingerConsumer->setDefaultMaxBufferCount(2);
 #else
     mSurfaceFlingerConsumer->setDefaultMaxBufferCount(3);
+
+#ifdef QCOM_BSP
+    char property[PROPERTY_VALUE_MAX];
+    if (property_get("sf.default.app_buffer_count", property, NULL) > 0) {
+        mSurfaceFlingerConsumer->setDefaultMaxBufferCount(atoi(property));
+    }
+#endif
+
 #endif
 
     const sp<const DisplayDevice> hw(mFlinger->getDefaultDisplayDevice());