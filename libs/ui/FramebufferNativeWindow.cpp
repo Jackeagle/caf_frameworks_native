edca1c870183a337033be038e6bd0f0492a11ca8
Vinu Deokaran <vinud@codeaurora.org>
2015-04-09 21:35:26 -0400

    ui: fix copyright headers
    Change-Id: Ie0890eec0f8118fec42ce89874c3ca0fa5228c2f

--- libs/ui/FramebufferNativeWindow.cpp
+++ libs/ui/FramebufferNativeWindow.cpp
@@ -1,6 +1,7 @@
 /*
 **
-** Copyright 2007, 2015 The Android Open Source Project
+** Copyright 2007 The Android Open Source Project
+** Copyright (c) 2015, The Linux Foundation. All rights reserved.
 **
 ** Licensed under the Apache License Version 2.0(the "License");
 ** you may not use this file except in compliance with the License.
